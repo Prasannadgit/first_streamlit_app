f2855d11e4cd0c43a4dcb942ebb319259da578db
Prasannadgit <113629810+Prasannadgit@users.noreply.github.com>
2022-09-18 18:05:50 +0530

    Update streamlit_app.py
    Breakfast menu added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My Parents new Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('1. Idly')
+streamlit.text('2. Pongal')
+streamlit.text('3. Dosa')
+strealit.text('4. Poori')
